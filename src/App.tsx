ad5f8ad1e5f13945a1f59a636d1997ceb7d5e4c8
Charmi-Malakiya <106948324+Charmi-Malakiya@users.noreply.github.com>
2023-07-27 17:44:26 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hellooooo</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
